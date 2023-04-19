2bfb07f75e4d1c95defe12e8f4216a463c251b0a
wyl <wyl@qq.com>
2023-04-19 23:10:38 +0800

    first commit

---
+++ src/main/java/com/gzeu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.gzeu.git;
+
+/**
+ * @Author WYL
+ * @Date 2023/4/19 22:59
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
