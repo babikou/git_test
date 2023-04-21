9ea16674011530811e53bff013181caa7cfd3d6c
wyl <wyl@qq.com>
2023-04-21 20:36:19 +0800

    hor-fix first commit

--- src/main/java/com/gzeu/git/GitTest.java
+++ src/main/java/com/gzeu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git");
         System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
